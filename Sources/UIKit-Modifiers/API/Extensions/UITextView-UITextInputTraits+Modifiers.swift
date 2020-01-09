fd82e99eb8d6b35de1f7b8a17857867e35e4b541
Christian Elies <chris.elies13@gmail.com>
2020-01-09 21:10:10 +0100

    feat(): implemented uitextinputtraits modifiers for textview and textfield

---
+++ Sources/UIKit-Modifiers/API/Extensions/UITextView-UITextInputTraits+Modifiers.swift
@@ -0,0 +1,88 @@
+//
+//  UITextInputTraits+Modifiers.swift
+//  
+//
+//  Created by Christian Elies on 09.01.20.
+//
+
+#if canImport(UIKit)
+import UIKit
+
+// MARK: - Configuring the Keyboard Appearance
+@available(macOS 10.15, *)
+public extension UITextView {
+    func keyboardType(_ keyboardType: UIKeyboardType) -> Self {
+        self.keyboardType = keyboardType
+        return self
+    }
+
+    func keyboardAppearance(_ keyboardAppearance: UIKeyboardAppearance) -> Self {
+        self.keyboardAppearance = keyboardAppearance
+        return self
+    }
+
+    func returnKeyType(_ returnKeyType: UIReturnKeyType) -> Self {
+        self.returnKeyType = returnKeyType
+        return self
+    }
+
+    @available(iOS 10, *)
+    func textContentType(_ textContentType: UITextContentType) -> Self {
+        self.textContentType = textContentType
+        return self
+    }
+}
+
+// MARK: - Managing the Keyboard Behavior
+@available(macOS 10.15, *)
+public extension UITextView {
+    func isSecureTextEntry(_ isSecureTextEntry: Bool) -> Self {
+        self.isSecureTextEntry = isSecureTextEntry
+        return self
+    }
+
+    func enablesReturnKeyAutomatically(_ enablesReturnKeyAutomatically: Bool) -> Self {
+        self.enablesReturnKeyAutomatically = enablesReturnKeyAutomatically
+        return self
+    }
+}
+
+// MARK: - Managing Spelling and Autocorrection
+@available(macOS 10.15, *)
+public extension UITextView {
+    func autocapitalizationType(_ autocapitalizationType: UITextAutocapitalizationType) -> Self {
+        self.autocapitalizationType = autocapitalizationType
+        return self
+    }
+
+    func autocorrectionType(_ autocorrectionType: UITextAutocorrectionType) -> Self {
+        self.autocorrectionType = autocorrectionType
+        return self
+    }
+
+    func spellCheckingType(_ spellCheckingType: UITextSpellCheckingType) -> Self {
+        self.spellCheckingType = spellCheckingType
+        return self
+    }
+}
+
+// MARK: - Configuring the Auto-Formatting Behaviors
+@available(iOS 11.0, *)
+@available(macOS 10.15, *)
+public extension UITextView {
+    func smartQuotesType(_ smartQuotesType: UITextSmartQuotesType) -> Self {
+        self.smartQuotesType = smartQuotesType
+        return self
+    }
+
+    func smartDashesType(_ smartDashesType: UITextSmartDashesType) -> Self {
+        self.smartDashesType = smartDashesType
+        return self
+    }
+
+    func smartInsertDeleteType(_ smartInsertDeleteType: UITextSmartInsertDeleteType) -> Self {
+        self.smartInsertDeleteType = smartInsertDeleteType
+        return self
+    }
+}
+#endif
